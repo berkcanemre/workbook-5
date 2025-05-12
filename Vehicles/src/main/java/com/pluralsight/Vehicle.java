d01f009120793fd3808985fe2955c56379ae7849
berkcanemre <berkcan.emre@gmail.com>
2025-05-12 11:59:37 -0400

    completed Part 2: Create Instances in Main

--- Vehicles/src/main/java/com/pluralsight/Vehicle.java
+++ Vehicles/src/main/java/com/pluralsight/Vehicle.java
@@ -39,4 +39,36 @@ public class Vehicle {
     public void setFuelCapacity(int fuelCapacity) {
         this.fuelCapacity = fuelCapacity;
     }
+//  Part 2
+    public static void main(String[] args) {
+        Moped slowRide = new Moped();
+        slowRide.setColor("Red");
+        slowRide.setFuelCapacity(5);
+        slowRide.setNumberOfPassengers(1);
+        slowRide.setCargoCapacity(10);
+
+        Car sedan = new Car();
+        sedan.setColor("Blue");
+        sedan.setFuelCapacity(15);
+        sedan.setNumberOfPassengers(5);
+        sedan.setCargoCapacity(200);
+
+        SemiTruck freightKing = new SemiTruck();
+        freightKing.setColor("White");
+        freightKing.setFuelCapacity(300);
+        freightKing.setNumberOfPassengers(2);
+        freightKing.setCargoCapacity(5000);
+
+        Hovercraft hover = new Hovercraft();
+        hover.setColor("Silver");
+        hover.setFuelCapacity(100);
+        hover.setNumberOfPassengers(4);
+        hover.setCargoCapacity(150);
+
+        // Output to test
+        System.out.println("Moped color: " + slowRide.getColor());
+        System.out.println("Car passengers: " + sedan.getNumberOfPassengers());
+        System.out.println("Truck cargo: " + freightKing.getCargoCapacity());
+        System.out.println("Hovercraft fuel: " + hover.getFuelCapacity());
+    }
 }
